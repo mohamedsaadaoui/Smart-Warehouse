9567539f23e29bc5900300440433b4c93bcccd86
Mohamed Saadaoui <mohamed.saadaoui.1@esprit.tn>
2026-07-23 19:08:25 +0100

    modification pom et add services

--- src/main/java/com/saadaoui/smartwarehouse/SmartWarehouseApplication.java
+++ src/main/java/com/saadaoui/smartwarehouse/SmartWarehouseApplication.java
@@ -1,4 +1,4 @@
-package com.SmartWarehouse.Smart.Warehouse;
+package com.saadaoui.smartwarehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
