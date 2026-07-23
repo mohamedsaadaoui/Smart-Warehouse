baad87d1f5c0d3db0f4cdb2c4503d068eb9b92f1
Mohamed Saadaoui <mohamed.saadaoui.1@esprit.tn>
2026-07-23 18:58:46 +0100

    modification pom et add services

---
+++ src/main/java/com/saadaoui/smartwarehouse/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.saadaoui.smartwarehouse.auth.security;
+
+import com.saadaoui.smartwarehouse.auth.entity.User;
+import com.saadaoui.smartwarehouse.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(
+                        user.getRoles()
+                                .stream()
+                                .map(role -> role.getName())
+                                .toArray(String[]::new)
+                )
+                .accountExpired(!user.getAccountNonExpired())
+                .accountLocked(!user.getAccountNonLocked())
+                .credentialsExpired(!user.getCredentialsNonExpired())
+                .disabled(!user.getEnabled())
+                .build();
+    }
+}
